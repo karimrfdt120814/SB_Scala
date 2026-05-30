2fcdab58990c8302bc155ab927c932f1e01d3185
Karimulla <karimrfdt120814@gmail.com>
2026-05-30 21:44:27 +0530

    Loops in scala

--- src/main/scala/Apr25Demo.scala
+++ src/main/scala/Apr25Demo.scala
@@ -1,7 +1,27 @@
 object Apr25Demo {
 
   def main(args: Array[String]):Unit={
-    println("Hello scala.")
+
+
+
+    val a = 10
+    val b=20
+    val c=5;
+
+    println(a>b)
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(a/b)
+    println(a%b)
+
+   // println("a = " + a);
+
+    if(a>b && a>c){
+      println("a is greater than b and c")
+      println("long highest value is " + Long.MaxValue)
+    }
+
   }
 
 }
--- src/main/scala/loops.scala
+++ src/main/scala/loops.scala
@@ -8,35 +8,63 @@ object loops {
     7 8 9 10
     11 12 13 14 15 */
 
-    var num =1
+//    var num =1
+//
+//    for(i <- 1 to 5){
+//      for(j <- 1 to i){
+//        print(num + " ")
+//        num = num+1
+//
+//      }
+//
+//    println("")
+//    }
+//
+//    for(i <- 1 to 5){
+//      for(j <- 1 to i){
+//        print(j+ " ")
+//      }
+//      println("")
+//    }
+//
+//
+//    for(i <- 1 to 5){
+//      for(j <- 1 to i) {
+//        print(i+ " ")
+//      }
+//      println("")
+//
+//
+//    }
+//
+//    for(i <- 1 to 5){
+//      for(j <- 1 to i) {
+//        print(i+ " ")
+//      }
+//      println("")
+//
+//
+//    }
+//
+//    var m =1
+//
+//    for(i <- 1 to 5) {
+//      for (j <- 1 to i) {
+//        print(m + " ")
+//        m = m + 1
+//
+//      }
+//      println("")
+//    }
 
-    for(i <- 1 to 5){
-      for(j <- 1 to i){
-        print(num + " ")
-        num = num+1
 
-      }
 
-    println("")
+var n =0
+    for(i <- 0 to 21){
+      n=n+i
+
     }
-
-    for(i <- 1 to 5){
-      for(j <- 1 to i){
-        print(j+ " ")
-      }
-      println("")
-    }
-
-
-    for(i <- 1 to 5){
-      for(j <- 1 to i) {
-        print(i+ " ")
-      }
-      println("")
-
-
-    }
-
+    println(n)
   }
 
 }
