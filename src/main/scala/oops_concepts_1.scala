d0a5bdc63144a44cbd1f851b8a68c4671f91c258
Karimulla <karimrfdt120814@gmail.com>
2026-06-03 21:56:43 +0530

    classes and instance creation

---
+++ src/main/scala/oops_concepts_1.scala
@@ -0,0 +1,36 @@
+
+class info {
+  val a =20
+  def add(num1:Int,num2:Int):Int={
+    num1+num2
+  }
+}
+
+class information(var x:Int, var y:Int) {
+
+  var a =10
+
+  def sum(a:Int,b:Int):Int={
+    a+b
+  }
+}
+
+
+
+
+
+
+object oops_concepts_1 {
+
+  def main(args:Array[String]): Unit = {
+
+    val karim = new information(100,200)
+
+    karim.a
+    karim.sum(10,20)
+
+
+
+  }
+
+}
