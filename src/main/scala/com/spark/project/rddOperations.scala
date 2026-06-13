f5c301a09e22a869368122840f26cc6c4c0c8932
Karimulla <karimrfdt120814@gmail.com>
2026-06-13 15:04:00 +0530

    spark rdd program

--- src/main/scala/com/spark/project/rddOperations.scala
+++ src/main/scala/com/spark/project/rddOperations.scala
@@ -7,15 +7,29 @@ object rddOperations {
   def main(args:Array[String]):Unit={
 
     val sc = new SparkContext("local[2]","rddOperations")
-    val arr = Array(1,2,3,3,4,4,5,5,6,7,8)
-    val input = sc.parallelize(arr)
-    val result=input.mean()
-    val resuld_dis=input.distinct()
-    val final_res=resuld_dis.collect
-
-    final_res.foreach(println)
+//    val arr = Array(1,2,3,3,4,4,5,5,6,7,8)
+//    val input = sc.parallelize(arr)
+//    val result=input.mean()
+//    val resuld_dis=input.distinct()
+//    val final_res=resuld_dis.collect
+//
+//    final_res.foreach(println)
 
     //println(result)
+
+    val myList = List(1,2,3,4,5,6,7,8,33)
+    val input = sc.parallelize(myList)
+    val myList1 = List(6,7,8,9,10,11,12,13)
+    val input1 = sc.parallelize(myList1)
+
+    val unionRdd = input.union(input1).collect()
+    val intersectRDD = input.intersection(input1).collect()
+    val subRdd = input.subtract(input1).collect()
+    val cartesianRdd = input.cartesian(input).collect()
+
+    cartesianRdd.foreach(println)
+
+
 
 
     scala.io.StdIn.readInt()
