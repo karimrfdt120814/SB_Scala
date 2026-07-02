8bf439087e19a847bc3f42867f572c5f6d6b7a64
Karimulla <karimrfdt120814@gmail.com>
2026-07-02 23:14:49 +0530

    dataframe

--- src/main/scala/com/spark/project/dataFrames.scala
+++ src/main/scala/com/spark/project/dataFrames.scala
@@ -1,5 +1,6 @@
 package com.spark.project
-
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.functions
 import org.apache.spark.sql.SparkSession
 
 
@@ -18,7 +19,37 @@ object dataFrames {
       .option("path","C:\\Users\\Asus\\Downloads/details-2026-04-05.csv")
       .load()
 
-    df.show(5,false)
+//    df.select(
+//      (col("id")+col("salary")).alias("addition")
+//    ).show(false)
+
+//    df.select(
+//      (col("id")>10) and (col("salary")>200)
+//    ).show(false)
+
+
+//    df.select(
+//      col("id"),
+//      col("name"),
+//      col("salary"),
+//      col("city"),
+//      when(col("salary")>500,"Rich").otherwise("Not Rich").alias("Status")
+//    ).show(false)
+
+    df.createOrReplaceTempView("myView")
+
+    spark.sql(
+      """
+        |select id,name,salary,city,
+        |case
+        |when salary > 500 then "Rich"
+        |else "Not Rich"
+        |end status
+        |from my view
+        |""".stripMargin
+    ).show(false)
+
+
 
 
   }
